cd963f4266c258a5d249d25c0314d8b4a07954f1
Christian <christian.lampl@gmx.at>
2020-12-02 22:26:06 -0300

    Add: a user can only edit/delete a story if he is the creator or if he is admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  has_many :stories, dependent: :destroy
+  has_many :stories
+
+  def authorized?(story)
+    admin? || story.user == self
+  end
 end
